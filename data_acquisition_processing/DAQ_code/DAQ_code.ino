e32cf6954e31e5c03652b9feaa1c34312dc8dab6
Tiffani Tjandra <tiffani.tjandra@rice.edu>
2019-01-20 20:17:04 -0600

    updated DAQ code

--- data_acquisition_processing/DAQ_code/DAQ_code.ino
+++ data_acquisition_processing/DAQ_code/DAQ_code.ino
@@ -13,17 +13,31 @@
  *  ToF SENSOR
  *  - SDA: 20
  *  - SCL: 21
+ *  Gyroscope
+ *  - Vin: 3.3V
+ *  - SDA: SDA1
+ *  - SCL: SCL1
  *  
  */
 
-
+//libraries for SD card reader
 #include <SPI.h>
 #include <SD.h>
+//library for ToF sensor
 #include "Adafruit_VL53L0X.h"
+//libraries for BNO055
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
 
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 
 File myFile;
+
+#define BNO055_SAMPLERATE_DELAY_MS (100)
+
+Adafruit_BNO055 bno = Adafruit_BNO055();
 
 const int buttonPin = 7;
 const int printPin = 8;
@@ -41,7 +55,6 @@ void setup() {
 
   //Initializing SD card reader
   Serial.print("Initializing SD card...");
-
   if (!SD.begin(53)) {
     Serial.println("initialization failed!");
     return;
@@ -57,6 +70,16 @@ void setup() {
   // power 
   Serial.println(F("initialization done.")); 
 
+  // Initializing BNO055 sensor
+  Serial.print("Initializing BNO055 sensor...");
+  if(!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+  Serial.println(F("initialization done"));
+
 }
 
 void loop() {
@@ -65,6 +88,8 @@ void loop() {
   int printState = digitalRead(printPin);
 
   VL53L0X_RangingMeasurementData_t measure;
+  
+  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
 
   //data collected will be stored as a string formatted as a CSV
   String dataString = "";
@@ -75,14 +100,21 @@ void loop() {
     //this for loop collects data from the 3 sensors 
     for (int i = 0; i < 3; i++){
       //collects data from ToF sensor
+      Serial.println(String(i));
       if (i == 0){
         lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
         float sensor = measure.RangeMilliMeter;
-        dataString += sensor;
-      } else{ //dummy code in place for 2 more sensors
+        dataString += String(sensor);
+      } 
+      if (i == 1){
+        float sensor1 = euler.y(); //pitch in degrees
+        float sensor2 = euler.z(); //roll in degrees
+        dataString += "," + String(sensor1) + "," + String(sensor2);
+      }
+      if (i == 2){ //dummy code in place for 1 more sensor
         dataString += ",";
-        float sensor = random(1,10);
-        dataString += sensor;
+        float sensor3 = random(1,10);
+        dataString += String(sensor3);
       }
     }
     //opening file on SD card and writing data string onto it
